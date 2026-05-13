77c04aead1f29e4f0dd22ec75c55b5acca160b69
Shelby Thompson <shelbytbaruch@gmail.com>
2026-05-13 00:27:44 +0000

    added dim agency mart

---
+++ models/groupwork/datamarts/dim_agency.sql
@@ -0,0 +1,12 @@
+WITH agencies AS (
+    SELECT DISTINCT
+        agency,
+        agency_name
+    FROM {{ ref('stg_311nyc_service_requests') }}
+    WHERE agency IS NOT NULL
+)
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['agency']) }} AS agency_key,
+    agency,
+    agency_name
+FROM agencies
