e4739e21ffa9484f45f7bf67ffe89ffdb2c26416
Shelby Thompson <shelbytbaruch@gmail.com>
2026-05-13 01:36:04 +0000

    fuck this

--- models/groupwork/marts/dim_complaint_type.sql
+++ models/groupwork/marts/dim_complaint_type.sql
@@ -1,18 +1,5 @@
-WITH complaint_types AS (
--- Complaint Type dimension (Sourced only from 311 data)
-WITH complaint_data AS (
-    SELECT DISTINCT
-        complaint_type,
-        COALESCE(descriptor, 'Unknown') AS descriptor
-    FROM {{ ref('stg_311nyc_service_requests') }}
-    WHERE complaint_type IS NOT NULL
-)
-
-SELECT
-    {{ dbt_utils.generate_surrogate_key([
-        'complaint_type',
-        'descriptor'
-    ]) }} AS complaint_type_key,
+SELECT DISTINCT
     complaint_type,
     descriptor
-FROM complaint_data
+FROM {{ ref('stg_311nyc_service_requests') }}
+WHERE complaint_type IS NOT NULL
