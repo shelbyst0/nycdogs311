fce49c374041b0b7092bd14f13709bf934e3e7de
Shelby Thompson <shelbytbaruch@gmail.com>
2026-05-13 01:51:33 +0000

    okay bitch

--- models/groupwork/marts/fact_dog_licensing.sql
+++ models/groupwork/marts/fact_dog_licensing.sql
@@ -1,11 +1,26 @@
 SELECT
-    animal_name,
-    animal_gender,
-    animal_birth_year,
-    breed_name,
-    owner_zipcode,
-    license_issued_date,
-    license_expired_date,
-    extract_year
+    dl.animal_name,
+    dl.animal_gender,
+    dl.animal_birth_year,
+    dl.breed_name,
+    dl.owner_zipcode,
+    dl.license_issued_date,
+    dl.license_expired_date,
+    dl.extract_year,
 
-FROM {{ ref('stg_nyc_dog_licensing') }}
+    -- Dimension keys
+    ac.animal_characteristics_key,
+    gb.gender_birth_key,
+
+    -- Dimension attributes
+    ac.size_category,
+    ac.temperament
+
+FROM {{ ref('stg_nyc_dog_licensing') }} dl
+
+LEFT JOIN {{ ref('dim_animal_characteristics') }} ac
+    ON dl.breed_name = ac.breed_name
+
+LEFT JOIN {{ ref('dim_gender_and_birth_year') }} gb
+    ON dl.animal_gender = gb.animal_gender
+   AND dl.animal_birth_year = gb.animal_birth_year
